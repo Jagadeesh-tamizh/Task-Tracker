49641acd4dfa605e31340146840dc3d4301ed9b9
Jagadeesh <jagadeeshtamizh@gmail.com>
2024-03-07 12:06:04 +0530

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,39 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+  {
+    id: 4,
+    text: 'Meeting at Work',
+    day: 'May 8th at 11:30pm',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Meeting at Home',
+    day: 'May 9th at 10:30pm',
+    reminder: false,
+  },
+  {
+    id: 6,
+    text: 'Meeting at Church',
+    day: 'May 10th at 9:30pm',
+    reminder: true,
+  },
+];
